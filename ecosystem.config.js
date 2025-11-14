a41406ecd14d9e34b6fd64a778800bfab27bfb93
c4lyp5o <calypso@calypsocloud.one>
2025-11-14 21:37:03 +0800

    fix: better ecosystem conf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,35 +1,34 @@
 module.exports = {
 	apps: [
 		{
-			name: "api-server",
+			name: "server",
 			script: "./src/server.js",
 			interpreter: "bun",
-
-			env_dev: {
+			instances: "max",
+			exec_mode: "cluster",
+			autorestart: true,
+			watch: false,
+			env: {
 				NODE_ENV: "development",
 				PORT: 5000,
 			},
-			env_prod: {
+			env_production: {
 				NODE_ENV: "production",
 				PORT: 5000,
-				instances: "max",
-				exec_mode: "cluster",
 			},
 		},
 		{
-			// --- Your Background Worker ---
-			name: "fanout-worker",
+			name: "worker",
 			script: "./src/worker.js",
 			interpreter: "bun",
-
 			instances: 1,
 			exec_mode: "fork",
-
-			// --- Environment-Specific Settings ---
-			env_dev: {
+			autorestart: true,
+			watch: false,
+			env: {
 				NODE_ENV: "development",
 			},
-			env_prod: {
+			env_production: {
 				NODE_ENV: "production",
 			},
 		},
